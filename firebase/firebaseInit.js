22ad08b9c090d86d43e70f2ef47731d541c3dd60
DeniseAreza <51269054+DeniseAreza@users.noreply.github.com>
2022-03-26 23:12:16 +0800

    error alert

--- firebase/firebaseInit.js
+++ firebase/firebaseInit.js
@@ -68,28 +68,20 @@ export function signUpUser (email, password) {
 
 // * for handling existing users
 export function loginUser (email, password) {
-  return new Promise(function(resolve, reject) {
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-    .then(function() {
-    // Existing and future Auth states are now persisted in the current
-    // session only. Closing the window would clear any existing state even
-    // if a user forgets to sign out.
-    // ...
-    // New sign-in will be persisted with session persistence.
-    return firebase.auth()
-                .signInWithEmailAndPassword(email, password)
-                .then(function()
-                {
-                    resolve();       
-                });
+  return new Promise (function (resolve, reject){
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      resolve();
+      // ...
     })
-    .catch(function(error) {
-    // Handle Errors here.
-    reject();
-    var errorCode = error.code;
-    var errorMessage = error.message;
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      reject();
     });
-})
+  })
 }
 // *
 
